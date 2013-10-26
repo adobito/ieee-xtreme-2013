db75b1d0cc6d335a06c74f929906da61726be3e4
Jesus E. Luzon Gonzalez <s802083255@ecenet.ece.uprm.edu>
2013-10-26 08:42:48 -0400

    Finished!

--- Problem_AJ/src/Solution.java
+++ Problem_AJ/src/Solution.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int amount = Integer.parseInt(in.nextLine());
+		String[] str = in.nextLine().trim().split(" ");
+		int[] speeds = new int[amount];
+		for(int i = 0; i < speeds.length; i++) {
+			speeds[i] = Integer.parseInt(str[i]);
+			
+		}
+		System.out.println(dp(0,speeds.length - 1, speeds));
 
 	}
 
+	private static int dp(int count, int index, int[] array) { {
+		if(index < 0) {
+			return count;
+		}
+		count = count + array[index];
+		return Math.max(count,dp(count,--index,array));
+	}
+		
+	}
 }
