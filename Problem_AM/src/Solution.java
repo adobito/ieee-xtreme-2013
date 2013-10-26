7f35220f16cebc08ae1c1e93b76517536b954e70
Jesus E. Luzon Gonzalez <s802083255@ecenet.ece.uprm.edu>
2013-10-26 04:38:41 -0400

    Initial commit and calculates risk

---
+++ Problem_AM/src/Solution.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int first = in.nextInt();
+		int second = in.nextInt();
+		in.nextLine();
+		int[][] arr = new int[first][second];
+		for(int i = 0; i < first; i++) {
+			for(int j = 0; j < second; j++) {
+				arr[i][j] = in.nextInt();
+			}
+		}
+		int value  = Integer.MAX_VALUE;
+		for(int i = 0; i < second; i++) {
+			value = Math.min(value, recursive(0,i,0,arr, new ArrayList<Pair>()));
+		}
+		System.out.println(value);
+		
+
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static int recursive(int x, int y, int count, int[][] arr,ArrayList<Pair> list) { 
+		int value = Integer.MAX_VALUE;
+		
+		if( y < 0 || y >= arr[0].length) {
+			return Integer.MAX_VALUE;
+		}
+		if(x == arr.length)
+			return count;
+		list.add(new Pair(x,y));
+		value = Math.min(value, recursive(x + 1, y - 1, count + arr[x][y],arr,(ArrayList<Pair>) list.clone()));
+		value = Math.min(value, recursive(x + 1, y, count + arr[x][y],arr,(ArrayList<Pair>) list.clone()));
+		value = Math.min(value, recursive(x + 1, y + 1, count + arr[x][y],arr,(ArrayList<Pair>) list.clone()));
+		return value;
+	}
+
+	static class Pair {
+		int x;
+		int y;
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public String toString() {
+			return "[" + x + "," + y + "]";
+		}
+	}
+}
