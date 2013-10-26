2f7e2791b5dc75ff9e5f3316c8c8eea16a474627
Jesus E. Luzon Gonzalez <s802083255@ecenet.ece.uprm.edu>
2013-10-26 12:41:34 -0400

    Initial commit.

--- Problem_AG/src/Solution.java
+++ Problem_AG/src/Solution.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()) {
+			String[] str = in.nextLine().split(" ");
+			if(str[0].equals("0") && str[0].equals("0")) {
+				break;
+			}
+			
+		}
 	}
-
+	
+	public static Thing recursive(int currX, int currY,boolean isDown, int turnCount, int numOfTurns, int n, Thing thing) {
+		
+		if(isDown) {
+			recursive(currX, currY + 1)
+		}
+		
+	}
+	static class Thing {
+		int count;
+		
+		public Thing() {
+			count = 0;
+		}
+		
+	}
 }
